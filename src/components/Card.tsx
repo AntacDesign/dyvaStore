6d80dde01afb471bdff5bf92bf72869edfbb74c8
Antac69 <angello_04_09@hotmail.com>
2024-06-08 02:29:42 -0500

    hero 65

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
 import React, { Children, type Key } from "react";
-import type { IndexType } from "typescript";
 interface propsCard {
   title?: Array<string>;
   body?: Array<string>;
@@ -21,48 +20,59 @@ export const Card = ({
 }: propsCard) => {
   if (type == 0) {
     return (
-      <li className="flex flex-col relative flex-1 overflow-hidden rounded-2xl min-h-40 max-h-72 box-border w-full">
+      /* card imagen */
+      <li className="flex flex-col relative flex-1 rounded-2xl h-[inherit] max-h-[inherit] box-border w-full">
+        {/* button card */}
         <div className="absolute bg-white pb-2 pr-2 rounded-br-3xl ">
+          {/* objetos que simulan corte */}
+          {/* arriba */}
           <div className="absolute pb-2 pr-2 block size-10 w-14 rounded-tr-none bg-transparent right-[-3.5em] rounded-full shadow-[-.6rem_-.9rem_0_white] z-[1]"></div>
+          {/* abajo */}
           <div className="absolute pb-2 pr-2 block size-10 w-14 rounded-bl-none bg-transparent bottom-[-72%] rounded-full shadow-[-.8rem_-.3rem_0_white] z-[1]"></div>
           <a
             href="#"
             className=" block rounded-full py-3 px-5 bg-gray-200 h-min z-10"
           >
+            {/* componente de iconAstro */}
             {children}
           </a>
         </div>
-        <div className="h-full w-full bg-gray-100 box-border">
-          <img src={img} alt="" className="bg-bottom
-          h-full w-full object-cover" />
+        {/* sección de imagen */}
+        <div className={imgCss}>
+          <img src={img} alt="" className="object-left-bottom h-full w-full object-cover rounded-2xl" />
         </div>
+        {/* body */}
         <p
           className="absolute text-xs w-full h-full flex
-          items-end px-3 pb-4"
+          items-end px-3 pb-4 empty:hidden"
         >
           {body}
         </p>
-        <div>
-          <p></p>
-        </div>
       </li>
     );
   } else if (type == 1) {
     return (
-      <li className="flex flex-col relative flex-1 rounded-2xl aspect-[4/3] box-border min-h-40 max-h-72">
+      /* card solo texto*/
+      <li className="flex flex-col relative flex-1 rounded-3xl aspect-[4/3] box-border min-h-40 max-h-72">
+        {/* sección topCard */}
         <div className="flex bg-white rounded-br-3xl">
+          <div className="relative">
+            {/* objeto que simulan corte */}
+            <div className="size-7 w-10 bg-transparent absolute right-[-.63rem] bottom-[-2px] rounded-br-3xl z-[1] border-2 border-t-0 border-s-0 shadow-[10px_8px_0_white]"></div>
+            {/* button card */}
             <a
               href="#"
               className="relative block rounded-full py-3 px-5 bg-gray-200 h-min z-10"
-            >
+              >
+              {/* componente iconoAstro */}
               {children}
             </a>
-          <div className="text-xs z-0 flex-1 border-2 p-2 box-border rounded-tl-2xl rounded-tr-2xl border-b-0 ml-2 h-14">
+                </div>
+          <div className="text-xs z-0 flex-1 border-2 p-2 box-border rounded-tl-3xl rounded-tr-3xl border-b-0 ml-2 h-14">
             <h2>{title}</h2>
           </div>
         </div>
-
-        <div className="h-full w-full text-xs border-2 rounded-2xl rounded-tr-none p-4 box-border">
+        <div className="h-full w-full text-xs border-2 rounded-3xl rounded-tr-none p-4 box-border">
           <p>{body}</p>
         </div>
         <div></div>
